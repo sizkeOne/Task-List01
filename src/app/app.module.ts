4882ca6f5fc296afb0f79f5ba5e3fb17420654b0
sizkeOne <gonzaloamoletto@live.com.ar>
2022-12-18 20:07:06 -0300

    Lista de tareas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtomComponent } from './components/buttom/buttom.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtomComponent
+    ButtomComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mock-task.ts
@@ -0,0 +1,42 @@
+
+
+ 
+import {Task} from "./task"
+
+export const TASKS: Task[] = [
+  {
+    id : 1,
+    text: "Recordarle a mi princesa que la amo",
+    day: 'Mañana',
+    reminder: false,
+  },
+  {
+    id: 2,
+    text: "Preparar la comida",
+    day: 'Lunes',
+    reminder: true,
+
+  },
+  {
+    id: 3,
+    text: "Hacerme tatuaje",
+    day: 'Viernes',
+    reminder: true,
+
+  },
+    {
+    id: 4,
+    text: "Ir a la peluqeria de sami",
+    day: 'Viernes',
+    reminder: true,
+
+  },
+  {
+    id: 5,
+    text: "Acariciar al Sr Gato",
+    day: 'Viernes',
+    reminder: true,
+
+  }
+
+]
