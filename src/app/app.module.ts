e0593a39a925b3e3f10a2d41766681a750e3a7ef
sizkeOne <gonzaloamoletto@live.com.ar>
2022-12-18 23:07:36 -0300

    actuali3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtomComponent } from './components/buttom/buttom.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { ItemCardComponent } from './components/item-card/item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ButtomComponent,
-    TasksComponent
+    TasksComponent,
+    ItemCardComponent
   ],
   imports: [
     BrowserModule
