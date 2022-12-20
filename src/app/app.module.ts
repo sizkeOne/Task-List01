9af04cdf5292ba74e60bffb1981441f5750079e7
sizkeOne <gonzaloamoletto@live.com.ar>
2022-12-20 00:19:22 -0300

    font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtomComponent } from './components/buttom/buttom.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { ItemCardComponent } from './components/item-card/item-card.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ItemCardComponent } from './components/item-card/item-card.component';
     ItemCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
